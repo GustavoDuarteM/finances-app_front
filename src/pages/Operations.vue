8802a0730f4f67879ecd93a8492b2916856ef38c
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-10-17 17:43:26 -0300

    Add navigation and title in operations page

--- src/pages/Operations.vue
+++ src/pages/Operations.vue
@@ -1,20 +1,36 @@
 <template>
   <v-container>
     <v-container>
-      <v-row no-gutters>
-        <v-card >
-          <OperationForm form_title="Novo Ganho" operation_flow="inflow">
-            <v-btn color="primary"> Adicionar Ganho </v-btn>
-          </OperationForm>
-          <OperationForm form_title="Novo Gasto" operation_flow="outflow">
-            <v-btn color="success"> Adicionar Gasto </v-btn>
-          </OperationForm>
-        </v-card>
-      </v-row>
+      <v-btn to="/painel" color="primary" text> Painel </v-btn>
+      /
+      <v-btn to="/painel" color="primary" text disabled> Transações </v-btn>
     </v-container>
-    
+    <v-card>
+      <v-container class="d-flex align-center">
+        <v-container>
+          <h1>Transações</h1>
+        </v-container>
+        <v-container class="d-flex justify-end">
+          <v-container>
+            <OperationForm form_title="Novo Ganho" operation_flow="inflow">
+              <v-btn color="primary"> Adicionar Ganho </v-btn>
+            </OperationForm>
+          </v-container>
+          <v-container>
+            <OperationForm
+              form_title="Novo Gasto"
+              operation_flow="outflow"
+              class="ml-15"
+            >
+              <v-btn color="success"> Adicionar Gasto </v-btn>
+            </OperationForm>
+          </v-container>
+        </v-container>
+      </v-container>
+    </v-card>
+
     <v-container>
-      <OperationsList/>
+      <OperationsList />
     </v-container>
   </v-container>
 </template>
@@ -25,8 +41,8 @@ import OperationsList from "../components/operations_list.vue";
 export default {
   components: {
     OperationForm,
-    OperationsList
-  }
+    OperationsList,
+  },
 };
 </script>
 
