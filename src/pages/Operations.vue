7bfb7fce2c6adb321767c2b6eed48c2afec5d2e5
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-11-07 11:18:40 -0300

    adjust documment format

--- src/pages/Operations.vue
+++ src/pages/Operations.vue
@@ -7,24 +7,23 @@
     </v-container>
     <v-card class="ma-3">
       <v-row>
-        <v-col md="6" sm="12" >
+        <v-col md="6" sm="12">
           <h1 class="pa-3">Transações</h1>
         </v-col>
         <v-col md="6" sm="12" class="d-flex align-center justify-space-around">
-            <OperationForm form_title="Novo Ganho" operation_flow="inflow">
-              <v-btn color="primary"> Adicionar Ganho </v-btn>
-            </OperationForm>
-            <OperationForm
-              form_title="Novo Gasto"
-              operation_flow="outflow"
-              class="ml-15"
-            >
-              <v-btn dark color="purple"> Adicionar Gasto </v-btn>
-            </OperationForm>
+          <OperationForm form_title="Novo Ganho" operation_flow="inflow">
+            <v-btn color="primary"> Adicionar Ganho </v-btn>
+          </OperationForm>
+          <OperationForm
+            form_title="Novo Gasto"
+            operation_flow="outflow"
+            class="ml-15"
+          >
+            <v-btn dark color="purple"> Adicionar Gasto </v-btn>
+          </OperationForm>
         </v-col>
       </v-row>
     </v-card>
-
     <v-container>
       <OperationsList />
     </v-container>
